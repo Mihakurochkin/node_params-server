4102be7f25c1b27b4cc66d2eec985a515f8ffbed
Mykhailo <kurochkinmy@gmail.com>
2025-09-08 11:47:11 +0300

    feat: implement params server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,30 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const params = new URL(req.url, 'http://localhost');
+    const parts = params.pathname.split('/').filter(Boolean);
+    const query = {};
+
+    for (const part of params.search.slice(1).split('&')) {
+      query[part.split('=')[0]] = part.split('=')[1];
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.write(
+      JSON.stringify({
+        parts,
+        query,
+      }),
+    );
+    res.end();
+  });
+
+  return server;
 }
 
 module.exports = {
